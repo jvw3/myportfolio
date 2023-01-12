ca31c28f7244c27a13684dcfe5e16f43470d8897
Jonathan Woodard <jvw.226@gmail.com>
2023-01-12 10:14:46 -0600

    created new app file

---
+++ src/Portfolio.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { PortfolioView } from "./components/views/PortfolioView";
+
+export const Portfolio = () => (
+  <>
+    <PortfolioView />
+  </>
+);
