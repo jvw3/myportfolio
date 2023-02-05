72f2b5aa321ff735291d02bbfd04d4fbb8024fa5
Jonathan Woodard <jvw.226@gmail.com>
2023-02-05 08:14:05 -0800

    updated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
+      xs: { min: "300px", max: "639px" },
+      sm: { min: "640px", max: "767px" },
+      md: { min: "768px", max: "1023px" },
+      lg: { min: "1024px", max: "1279px" },
+      xl: { min: "1280px", max: "1535px" },
       xxl: "1536px",
     },
     extend: {
@@ -14,8 +15,38 @@ module.exports = {
         homebackground: "url(../src/images/portfoliobackground.jpg)",
         buttonbackground1: "url(../src/images/buttonback2.jpg)",
       },
+      height: {
+        89: "89vh",
+        75: "75vh",
+        40: "40vh",
+        80: "80vh",
+      },
       colors: {
-        "contentbackground": "#000053"
+        contentbackground: "#000053",
+      },
+      keyframes: {
+        appear: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        disappear: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+        slide: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+        slide2: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+      },
+      animation: {
+        appear: "appear 0.25s ease-in-out",
+        disappear: "disappear 0.25s ease-in-out",
+        slide: "slide 30s infinite linear",
+        slide2: "slide2 30s infinite linear",
       },
     },
   },
