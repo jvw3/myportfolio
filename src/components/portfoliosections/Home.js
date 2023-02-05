2dfc4ed171a25052955219640e9f37544639506a
Jonathan Woodard <jvw.226@gmail.com>
2023-02-05 08:16:00 -0800

    created home page for portfolio

--- src/components/portfoliosections/Home.js
+++ src/components/portfoliosections/Home.js
@@ -15,17 +15,17 @@ export const Home = () => {
     return (
       <>
         <main className="flex-col">
-          <div className=" bg-homebackground w-screen h-screen bg-cover">
-            <div className="bg-gradient-to-b from-transparent via-transparent to-contentbackground  w-screen h-screen bg-cover flex-col justify-between">
+          <div className="w-screen h-screen bg-center xxl:bg-cover xl:bg-cover lg:bg-cover md:bg-cover sm:bg-cover xs:bg-cover bg-homebackground">
+            <div className="flex-col justify-between w-screen h-screen bg-cover bg-gradient-to-b from-transparent via-transparent to-contentbackground">
               <div className="pt-3 pl-3">
                 <img className="w-24" src={Logo} />
               </div>
-              <div className="pl-10 flex justify-between w-full h-2/4">
-                <h1 className="mt-28 text-6xl text-white w-1/3">
+              <div className="flex justify-between w-full pl-10 h-2/4">
+                <h1 className="mt-10 text-white lg:w-60 lg:text-6xl xl:text-6xl xl:w-64 md:text-5xl xxl:text-7xl xxl:w-76">
                   Hi there, My name is Jonathan Woodard{" "}
                 </h1>
                 <img
-                  className="w-2/12 h-3/5 rounded-full mr-20 mt-10"
+                  className="mt-10 mr-20 rounded-full xs:w-32 xs:h-32 xs:mr-8 sm:w-32 sm:h-32 md:w-48 md:h-48 lg:w-68 lg:h-72 xl:w-68 xl:h-72 xxl:w-68 xxl:h-72"
                   src={profileImage}
                 />
               </div>
@@ -34,19 +34,19 @@ export const Home = () => {
               </div>
             </div>
           </div>
-          <div className="bg-contentbackground h-screen w-screen flex justify-center items-center">
+          <div className="flex items-center justify-center w-screen h-75 bg-contentbackground">
             <About />
           </div>
-          <div className="bg-contentbackground h-screen w-screen flex justify-center items-center">
+          <div className="flex items-center justify-center w-screen h-75 bg-contentbackground">
             <Experience />
           </div>
-          <div className="bg-contentbackground h-screen w-screen flex justify-center items-center">
+          <div className="flex items-center justify-center w-screen h-75 bg-contentbackground">
             <Skills />
           </div>
-          <div className="bg-contentbackground h-screen w-screen flex justify-center items-center">
+          <div className="flex items-center justify-center w-screen h-75 bg-contentbackground">
             <Work />
           </div>
-          <div className="bg-contentbackground h-screen w-screen flex justify-center items-center">
+          <div className="flex items-center justify-center w-screen h-screen bg-contentbackground">
             <Contact />
           </div>
         </main>
