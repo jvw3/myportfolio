27fe35d3d3fbb73cd1ee6ff9e226ad40b438fe9c
Jonathan Woodard <jvw.226@gmail.com>
2023-02-05 08:14:27 -0800

    created about section in portfolio

--- src/components/portfoliosections/About.js
+++ src/components/portfoliosections/About.js
@@ -3,9 +3,17 @@
 export const About = () => {
   return (
     <>
-      <main className="">
-        <h2 className="text-white text-4xl">About</h2>
-        <p></p>
+      <main className="flex-col">
+        <h2 className="mb-10 ml-20 text-6xl text-white">About</h2>
+        <p className="w-1/2 ml-40 text-white xl:text-2xl xxl:text-3xl">
+          I'm a self-motivated, team oriented, algorithmic thinker with a
+          passion for learning, and a strength for working in a high pressure,
+          fast-paced, environment. I've recently made the transition to software
+          development after playing professional football for the last seven
+          years. I'm accustomed to comprehending a variety of new concepts in a
+          short period of time and seeking a position to expand and leverage my
+          strengths.
+        </p>
       </main>
     </>
   );
