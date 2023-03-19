deaffee3331b1c71b9c11ae65ab7a53ba876b4e6
Jonathan Woodard <jvw.226@gmail.com>
2023-03-19 18:22:41 -0500

    refactored about section

--- src/components/portfoliosections/About.js
+++ src/components/portfoliosections/About.js
@@ -4,7 +4,7 @@ import { useInView } from "react-intersection-observer";
 // import { useInView } from "framer-motion";
 // import { useRef } from "react";
 
-export const About = () => {
+export const About = ({aboutNavRef}) => {
   const options = { threshold: 0.4, triggerOnce: true };
   const { ref: aboutRef, inView: aboutVisible } = useInView(options);
 
@@ -13,13 +13,19 @@ export const About = () => {
     <>
       <main
         ref={aboutRef}
-        className={`flex-col ml-20 ${aboutVisible ? "animate-appear" : "invisible"}`}
+        myref={aboutNavRef}
+        className={`flex-col ml-20 ${
+          aboutVisible ? "animate-appear" : "invisible"
+        }`}
       >
         <h2 className=" text-6xl text-white font-mono">About</h2>
-        <div className="flex ml-20 mt-10">
-          <img className="relative w-1/5 z-10 rounded-3xl" src={profilePic}></img>
+        <div className="flex md:flex-col ml-20 mt-10">
+          <img
+            className="relative w-[300px] md:w-[300px] md:h-[400px] z-10 rounded-3xl"
+            src={profilePic}
+          ></img>
           <div className="flex flex-col">
-            <div className="ml-40 mb-10">
+            <div className="lg:ml-40 xl:ml-40 md:mt-10 xxl:ml-40 mb-10">
               <a
                 className="bg-purpleyblueimage bg-cover p-3 rounded-lg w-fit text-white"
                 href={resume}
@@ -29,7 +35,7 @@ export const About = () => {
                 My resume
               </a>
             </div>
-            <p className="w-1/2 ml-40 text-white xl:text-2xl xxl:text-2xl font-spacemono">
+            <p className="w-1/2 md:3/4 lg:ml-40 xl:ml-40 xxl:ml-40 text-white xl:text-2xl xxl:text-2xl font-spacemono">
               I'm a self-motivated, team oriented, algorithmic thinker with a
               passion for learning, and a strength for working in a high
               pressure, fast-paced, environment. I've recently made the
