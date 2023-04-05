d8bd0db3fe8e133262ed7e4882aa59cd55f4927c
Jonathan Woodard <jvw.226@gmail.com>
2023-04-05 11:23:06 -0500

    updated animations in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,13 +46,9 @@ module.exports = {
           "0%": { transform: "translateX(-100%)" },
           "100%": { transform: "translateX(0%)" },
         },
-        slideIn: {
-          "0%": { transform: "translateX(-100%)" },
-          "100%": { transform: "translateX(0%)" },
-        },
         slide2: {
           "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
       animation: {
